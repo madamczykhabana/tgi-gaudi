d13112753bdbea5c2aac1b489a9cc0db369f9221
Michal Adamczyk <madamczyk@habana.ai>
2024-02-23 17:15:03 +0200

    Test

--- server/text_generation_server/models/causal_lm.py
+++ server/text_generation_server/models/causal_lm.py
@@ -113,11 +113,7 @@ def grouped_shift(tensor_groups, dims, offset, merge_graphs):
     return tensor_groups
 
 
-def flatten(seq):
-    return type(seq)(itertools.chain(*seq))
-
-
-def move(dst_tensors, dst_indices, src_tensors, max_tensors_in_graph):
+def move(dst_tensors, dst_indices, src_tensors):
     bs_dim = 0
     num_indices = dst_indices.size(0)
     if len(src_tensors) < len(dst_tensors):
@@ -125,22 +121,16 @@ def move(dst_tensors, dst_indices, src_tensors, max_tensors_in_graph):
         assert src_tensors[0].size(0) == len(dst_tensors), "Batched dim shape mismatch!"
         # Source tensors are merged. We'll iterate over the first (stacked) dim
         src_tensors = src_tensors[0]
-    #for i, (dst_t, src_t) in enumerate(zip(dst_tensors, src_tensors)):
-    for i in range(len(dst_tensors)):
-        #print(i, type(dst_tensors), type(src_tensors))
-        if i % max_tensors_in_graph == 0:
-            htorch.core.mark_step()
-        dst_t = dst_tensors[i]
-        src_t = src_tensors[i]
+    for i, (dst_t, src_t) in enumerate(zip(dst_tensors, src_tensors)):
         if src_t.size(bs_dim) != num_indices:
             src_t = torch.narrow(src_t, bs_dim, 0, num_indices)
         dst_t.index_copy_(bs_dim, dst_indices, src_t)
     htorch.core.mark_step()
 
 
-def grouped_move(dst_tensor_groups, dst_indices, src_tensor_groups, max_tensors_in_graph):
+def grouped_move(dst_tensor_groups, dst_indices, src_tensor_groups):
     for dst_tensors, src_tensors in zip(dst_tensor_groups, src_tensor_groups):
-        move(dst_tensors, dst_indices, src_tensors, max_tensors_in_graph)
+        move(dst_tensors, dst_indices, src_tensors)
 
 
 def extend_tensor(tensor, padding, dim):
@@ -329,8 +319,7 @@ class CausalLMBatch(Batch):
         for src_b in src_batches:
             dst_indices = to_tensor_indices(src_b.update_indices(free_indices_gen), self.input_ids.device)
             src_tensors, _, src_dims = src_b.get_tensor_groups()
-            max_tensors_in_graph = 20
-            grouped_move(dst_tensors, dst_indices, src_tensors, max_tensors_in_graph)
+            grouped_move(dst_tensors, dst_indices, src_tensors)
         self.set_tensor_groups(dst_tensors)
 
     @classmethod
